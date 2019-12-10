0fd96e4fdb19371dc8dc09399dbc122afe49c131
Elijah Soria <elijahsoria@google.com>
2019-12-10 14:45:24 -0800

    Implement decryption

--- build-system/server/golang/crypto.go
+++ build-system/server/golang/crypto.go
@@ -1,23 +1,76 @@
 package golang
 
 import (
-	"context"
+	"encoding/base64"
+	"encoding/json"
 	"log"
-	cloudkms "cloud.google.com/go/kms/apiv1"
-	"github.com/google/tink/go/insecurecleartextkeyset"
-    "github.com/google/tink/go/keyset"
+	"github.com/google/tink/go/aead"
+	"github.com/google/tink/go/hybrid"
+	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/integration/gcpkms"
+	"github.com/google/tink/go/keyset"
 )
 
 const gcsProjectId string = "";
 const gcsLocationId string = ""; 
 const gcsMasterKeyLocation string = "";
 
-func Decrypt() {
-	// Create the KMS client.
-	ctx := context.Background()
-	client, err := cloudkms.NewKeyManagementClient(ctx)
+type swgEncryptionKey struct {
+	AccessRequirements []string
+	Key                string
+}
+
+
+func Decrypt(input string) string {
+	keyURI := fmt.Sprintf(
+		"gcp-kms://projects/%s/locations/%s/keyRings/%s/cryptoKeys/%s",
+		*project,
+		*location,
+		*keyring,
+		*key)
+
+	gcpclient, err := gcpkms.NewGCPClient(keyURI)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
-	khPub, err := insecurecleartextkeyset.Read(keyset.NewBinaryReader(bytes.NewReader(ksPub)))
+
+	//_, err = gcpclient.LoadCredentials(*credentialsFile)
+	_, err = gcpclient.LoadDefaultCredentials()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	registry.RegisterKMSClient(gcpclient)
+
+	priv := hybrid.ECIESHKDFAES128GCMKeyTemplate()
+	khPriv, err := keyset.NewHandle(aead.KMSEnvelopeAEADKeyTemplate(keyURI, priv))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hd, err := hybrid.NewHybridDecrypt(khPriv)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := base64.StdEncoding.DecodeString(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dec, err := hd.Decrypt(b, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var dat swgEncryptionKey
+	if err = json.Unmarshal(dec, &dat); err != nil {
+        log.Fatal(err)
+	}
+	var decDocumentKey string = "";
+	for _, val := range dat.AccessRequirements {
+		if (val == "scenic-2017.appspot.com:premium") {
+			decDocumentKey = dat.Key
+		}
+	}
+	return decDocumentKey
 }
