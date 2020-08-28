2556a8cde7f0815ae9d905fae70523f48b7bbc65
Chris Antaki <cfa@google.com>
2020-08-28 09:18:15 -0700

    Fixes formatting again

--- app/js/metering.js
+++ app/js/metering.js
@@ -4,38 +4,38 @@ export const MeteringDemo = {
   setupControls: () => {
     // Wire up reset button.
     document
-      .querySelector("#metering-controls button")
-      .addEventListener("click", () => {
+        .querySelector('#metering-controls button')
+        .addEventListener('click', () => {
         // Forget the existing PPID.
-        delete localStorage.meteringStateId;
+          delete localStorage.meteringStateId;
 
-        // Refresh so a new PPID will be created and used.
-        window.location.reload();
-      });
+          // Refresh so a new PPID will be created and used.
+          window.location.reload();
+        });
 
     // Show reset button.
-    document.body.classList.add("metering");
+    document.body.classList.add('metering');
 
     // Update nav button to carry over full URL query.
-    document.querySelectorAll("header .nav-button").forEach((navButton) => {
+    document.querySelectorAll('header .nav-button').forEach(navButton => {
       navButton.href = navButton.href.replace(/\?.*/, location.search);
     });
   },
 
   /** Returns a new Publisher Provided ID (PPID) suitable for demo purposes. */
-  createPpid: () => "ppid" + Math.round(Math.random() * 9999999999999999),
+  createPpid: () => 'ppid' + Math.round(Math.random() * 9999999999999999),
 
   /** Returns a Publisher Provided ID (PPID) suitable for demo purposes. */
   getPpid: () => {
     if (!localStorage.meteringStateId) {
       localStorage.meteringStateId = MeteringDemo.createPpid();
     }
-    console.log("Metering PPID: " + localStorage.meteringStateId);
+    console.log('Metering PPID: ' + localStorage.meteringStateId);
     return localStorage.meteringStateId;
   },
 
   /** Opens the paywall for demo purposes. */
   openPaywall: () => {
-    document.documentElement.classList.add("open-paywall");
+    document.documentElement.classList.add('open-paywall');
   },
 };
